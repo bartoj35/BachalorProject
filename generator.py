99f5b6e85fb0b3a87fc5f70134fdffd9fcc5b414
Jakub Barton <bartoj35@fit.cvut.cz>
2023-01-28 15:49:47 +0100

    Modify generator for more generic calls

--- generator.py
+++ generator.py
@@ -7,52 +7,47 @@ RIGHT_CURLY = '}'
 FILE_COUNT = 2
 
 
-files = [open(str(input(f'Enter name for file{i}: ')) + '.c', 'w') for i in range(FILE_COUNT)]
+file = open(str(input(f'Enter name for the test file: ') + '.test'), 'w')
 n = int(input('Enter disjoint set size: '))
 
 UNION_COUNT = 3 * n
 FIND_COUNT = 2 * n
 
-def beggining(file):
-	file.write(f'void test ( ) {NEWLINE}')
+def beggining():
+	file.write(f'void test ( ) {LEFT_CURLY}{NEWLINE}')
 	file.write(f'{TAB}DisjointSet * set = NULL;{NEWLINE}')
 	file.write(f'{TAB}for ( int i = 0; i < {n}; i ++ ) {LEFT_CURLY}{NEWLINE}')
 	file.write(f'{TAB}{TAB}makeSet ( i, & set );{NEWLINE}')
 	file.write(f'{TAB}{RIGHT_CURLY}{NEWLINE}')
 	file.write(f'{NEWLINE}')
 
-def unions(files):
+def unions():
 	for i in range(UNION_COUNT):
 		a, b = random.randint(0, n - 1), random.randint(0, n - 1)
-		for j in range(FILE_COUNT):
-			files[j].write(f'{TAB}unionSet ( {a}, {b}, & set );{NEWLINE}')
+		file.write(f'{TAB}unionSet ( {a}, {b}, & set );{NEWLINE}')
 
-def value_var(file):
+def finds():
+	for i in range(FIND_COUNT):
+		a = random.randint(0, n - 1)
+		file.write(f'{TAB}find ( {a}, ARG, & value );{NEWLINE}')
+
+
+
+def value_var():
 	file.write(f'{NEWLINE}')
 	file.write(f'{TAB}int value = 0;{NEWLINE}')
 
-def ending(file):
+def ending():
 	file.write(f'{NEWLINE}')
+	file.write(f'{TAB}print ( set );{NEWLINE}')
 	file.write(f'{TAB}freeSet ( set );{NEWLINE}')
-	file.write(f'{NEWLINE}')
-	file.write(f'{TAB}return 0;{NEWLINE}')
 	file.write('}\n')
 	file.close()
 
 
-for i in range(FILE_COUNT):
-	beggining(files[i])
-
-unions(files)
-
-for i in range(FILE_COUNT):
-	value_var(files[i])
-
-for i in range(FIND_COUNT):
-	a = random.randint(0, n - 1)
-	files[0].write(f'{TAB}find ( {a}, set, & value );{NEWLINE}')
-	files[1].write(f'{TAB}find ( {a}, & set, & value );{NEWLINE}')
-
-for i in range(FILE_COUNT):
-	ending(files[i])
+beggining()
+unions()
+value_var()
+finds()
+ending()
 
