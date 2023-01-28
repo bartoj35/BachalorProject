5bc9e4597075f034d719c4a0b6b5ff9f72e99253
Jakub Barton <bartoj35@fit.cvut.cz>
2023-01-28 16:03:18 +0100

    Remove not nice Python constructs

--- generator.py
+++ generator.py
@@ -8,40 +8,39 @@ FILE_COUNT = 2
 
 
 file = open(str(input(f'Enter name for the test file: ') + '.test'), 'w')
-n = int(input('Enter disjoint set size: '))
+set_size = int(input('Enter disjoint set size: '))
 
-UNION_COUNT = 3 * n
-FIND_COUNT = 2 * n
+
+UNION_COUNT = 3 * set_size
+FIND_COUNT = 2 * set_size
+
 
 def beggining():
-	file.write(f'void test ( ) {LEFT_CURLY}{NEWLINE}')
-	file.write(f'{TAB}DisjointSet * set = NULL;{NEWLINE}')
-	file.write(f'{TAB}for ( int i = 0; i < {n}; i ++ ) {LEFT_CURLY}{NEWLINE}')
-	file.write(f'{TAB}{TAB}makeSet ( i, & set );{NEWLINE}')
-	file.write(f'{TAB}{RIGHT_CURLY}{NEWLINE}')
-	file.write(f'{NEWLINE}')
+	file.write('\tDisjointSet * set = NULL;\n')
+	file.write(f'\tfor ( int i = 0; i < {set_size}' + '; i ++ ) {\n')
+	file.write('\t\tmakeSet ( i, & set );\n')
+	file.write('\t}\n\n')
+
 
 def unions():
 	for i in range(UNION_COUNT):
-		a, b = random.randint(0, n - 1), random.randint(0, n - 1)
-		file.write(f'{TAB}unionSet ( {a}, {b}, & set );{NEWLINE}')
+		a, b = random.randint(0, set_size - 1), random.randint(0, set_size - 1)
+		file.write(f'\tunionSet ( {a}, {b}, & set );\n')
+
 
 def finds():
 	for i in range(FIND_COUNT):
-		a = random.randint(0, n - 1)
-		file.write(f'{TAB}find ( {a}, ARG, & value );{NEWLINE}')
-
+		a = random.randint(0, set_size - 1)
+		file.write(f'\tfind ( {a}, ARG, & value );\n')
 
 
 def value_var():
-	file.write(f'{NEWLINE}')
-	file.write(f'{TAB}int value = 0;{NEWLINE}')
+	file.write(f'\n\tint value = 0;\n')
+
 
 def ending():
-	file.write(f'{NEWLINE}')
-	file.write(f'{TAB}print ( set );{NEWLINE}')
-	file.write(f'{TAB}freeSet ( set );{NEWLINE}')
-	file.write('}\n')
+	file.write(f'\n\tprint ( set );\n')
+	file.write(f'\tfreeSet ( set );\n')
 	file.close()
 
 
