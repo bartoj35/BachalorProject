29f370cdd5949fdec9ab44ee1b62a6acc1ca2d0a
Jakub Barton <bartoj35@fit.cvut.cz>
2023-01-28 16:12:35 +0100

    Create fuzzer for generating test cases other way then generator.py

---
+++ fuzzer.py
@@ -0,0 +1,46 @@
+import random
+
+file = open(str(input(f'Enter name for the test file: ')), 'w')
+set_size = int(input('Enter disjoint set size: '))
+call_count = int(input('How many times should be operation called on set? '))
+
+
+def beggining():
+	file.write('\tDisjointSet * set = NULL;\n')
+	file.write(f'\tfor ( int i = 0; i < {set_size}' + '; i ++ ) {\n')
+	file.write('\t\tmakeSet ( i, & set );\n')
+	file.write('\t}\n\n')
+
+
+def union():
+	a, b = random.randint(0, set_size - 1), random.randint(0, set_size - 1)
+	file.write(f'\tunionSet ( {a}, {b}, & set );\n')
+
+
+def find():
+	a = random.randint(0, set_size - 1)
+	file.write(f'\tfind ( {a}, ARG, & value );\n')
+
+
+def value_var():
+	file.write(f'\tint value = 0;\n')
+
+
+def ending():
+	file.write(f'\n\tprint ( set );\n')
+	file.write(f'\tfreeSet ( set );\n')
+	file.close()
+
+
+beggining()
+value_var()
+
+for i in range(0,call_count):
+	decide: int = random.randint(0,3)
+	if decide == 0:
+		find()
+	else:
+		union()
+
+ending()
+
